353245e7714c5d56d8b3b4f896c17f464cedd5fe
aidansiegel <86203970+aidansiegel@users.noreply.github.com>
2025-10-19 09:47:09 -0400

    Create index.ts

---
+++ packages/core/src/index.ts
@@ -0,0 +1,6 @@
+export type { Options } from "./core";
+export { generateIdentipattern } from "./core";
+export { toSvg } from "./facades/svg";
+export { update } from "./facades/dom";
+export { toCanvas } from "./facades/canvas";
+export { IdentipatternIcon } from "./react";
